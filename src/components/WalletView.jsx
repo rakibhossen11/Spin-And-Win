db7a861d12a4ce18651051f5b798788c109c5755
rakibhossen <rakibhossen1660@gmail.com>
2024-01-30 19:01:10 +0600

    app view show

--- src/components/WalletView.jsx
+++ src/components/WalletView.jsx
@@ -1,32 +1,54 @@
-import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
-import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Animated from "react-native-reanimated";
+import { View, Text, StyleSheet } from "react-native";
+import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
+import { ReText } from "react-native-redash";
+import Animated, {
+  interpolate,
+  interpolateColor,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import { LinearGradient } from "expo-linear-gradient";
 import { SCREEN_WIDTH } from "../constants/screen";
 
 const AnimatedGradient = Animated.createAnimatedComponent(LinearGradient);
 
-const WalletView = () => {
+const WalletView = ({ walletBalance, opacity, amountText }) => {
+  const labelStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
+
+  const walletContentAStyle = useAnimatedStyle(() => {
+    return {
+      borderColor: interpolateColor(
+        opacity.value,
+        [0, 1],
+        ["rgba(209, 177, 177,0.4)", "#D1A15F"]
+      ),
+      borderWidth: interpolate(opacity.value, [0, 1], [1, 2]),
+    };
+  });
+
   return (
-    <View style={styles.container}>
+    <View style={styles.walletView}>
       <AnimatedGradient
         colors={["#FFEAA2", "#ECB65B"]}
-        style={[styles.labelWallet]}
+        style={[labelStyle, styles.walletLabel]}
       >
         <FontAwesome name="dollar" size={18} />
-        <Text style={styles.walletLabelText}>30</Text>
+        <Text style={styles.walletLabelText}>{walletBalance}</Text>
       </AnimatedGradient>
       <AnimatedGradient
+        style={[styles.walletContent, walletContentAStyle]}
         colors={["#303053", "#4C4A5F"]}
-        style={[styles.walletContent]}
       >
         <FontAwesome5 name="coins" size={50} color="white" />
         <View style={styles.walletTextContainer}>
-          <Text style={styles.addToWalletText}>Added To Wallet</Text>
+          <Text style={styles.addedToWalletText}>Added To Wallet</Text>
           <View style={styles.walletAmountContainer}>
-            <FontAwesome name="dollar" size={24} color={"white"} />
-            <Text style={styles.walletAmountText}>45</Text>
+            <FontAwesome name="dollar" size={24} color="white" />
+            <ReText text={amountText} style={styles.walletAmountText} />
           </View>
         </View>
       </AnimatedGradient>
@@ -34,12 +56,14 @@ const WalletView = () => {
   );
 };
 
+export default WalletView;
+
 const styles = StyleSheet.create({
-  container: {
+  walletView: {
     position: "relative",
     marginTop: 30,
   },
-  labelWallet: {
+  walletLabel: {
     flexDirection: "row",
     justifyContent: "center",
     columnGap: 4,
@@ -71,19 +95,108 @@ const styles = StyleSheet.create({
   walletTextContainer: {
     rowGap: 8,
   },
-  addToWalletText: {
-    color: 'white',
+  addedToWalletText: {
+    color: "white",
   },
   walletAmountContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     columnGap: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
   walletAmountText: {
-    color: 'white',
+    color: "white",
     fontSize: 24,
-    fontWeight: 'bold',
-  }
+    fontWeight: "bold",
+  },
 });
 
-export default WalletView;
+
+// import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
+// import { LinearGradient } from "expo-linear-gradient";
+// import React from "react";
+// import { StyleSheet, Text, View } from "react-native";
+// import Animated from "react-native-reanimated";
+// import { SCREEN_WIDTH } from "../constants/screen";
+
+// const AnimatedGradient = Animated.createAnimatedComponent(LinearGradient);
+
+// const WalletView = () => {
+//   return (
+//     <View style={styles.container}>
+//       <AnimatedGradient
+//         colors={["#FFEAA2", "#ECB65B"]}
+//         style={[styles.labelWallet]}
+//       >
+//         <FontAwesome name="dollar" size={18} />
+//         <Text style={styles.walletLabelText}>30</Text>
+//       </AnimatedGradient>
+//       <AnimatedGradient
+//         colors={["#303053", "#4C4A5F"]}
+//         style={[styles.walletContent]}
+//       >
+//         <FontAwesome5 name="coins" size={50} color="white" />
+//         <View style={styles.walletTextContainer}>
+//           <Text style={styles.addToWalletText}>Added To Wallet</Text>
+//           <View style={styles.walletAmountContainer}>
+//             <FontAwesome name="dollar" size={24} color={"white"} />
+//             <Text style={styles.walletAmountText}>45</Text>
+//           </View>
+//         </View>
+//       </AnimatedGradient>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     position: "relative",
+//     marginTop: 30,
+//   },
+//   labelWallet: {
+//     flexDirection: "row",
+//     justifyContent: "center",
+//     columnGap: 4,
+//     alignItems: "center",
+//     position: "absolute",
+//     top: -15,
+//     zIndex: 2,
+//     width: 100,
+//     left: (SCREEN_WIDTH / 2 - 100) / 2,
+//     borderRadius: 50,
+//     paddingHorizontal: 12,
+//     height: 35,
+//   },
+//   walletLabelText: {
+//     fontSize: 18,
+//     fontWeight: "500",
+//   },
+//   walletContent: {
+//     width: SCREEN_WIDTH / 2,
+//     height: 110,
+//     borderColor: "rgba(209, 177, 177,0.4)",
+//     borderWidth: 1,
+//     borderRadius: 8,
+//     flexDirection: "row",
+//     justifyContent: "space-between",
+//     alignItems: "center",
+//     padding: 10,
+//   },
+//   walletTextContainer: {
+//     rowGap: 8,
+//   },
+//   addToWalletText: {
+//     color: 'white',
+//   },
+//   walletAmountContainer: {
+//     flexDirection: 'row',
+//     columnGap: 10,
+//     alignItems: 'center',
+//   },
+//   walletAmountText: {
+//     color: 'white',
+//     fontSize: 24,
+//     fontWeight: 'bold',
+//   }
+// });
+
+// export default WalletView;
---
+++ src/utlis/Path.js
@@ -0,0 +1,78 @@
+import { SCREEN_WIDTH } from "../constants/screen";
+import { line, arc as d3Arc } from "d3-shape";
+
+const generateBowShape = () => {
+  const radius = SCREEN_WIDTH / 6;
+  const angleRange = (Math.PI * 2) / 3;
+  const numPoints = 25;
+
+  const points = Array.from({ length: numPoints }, (_, i) => {
+    const angle =
+      (i / (numPoints - 1)) * angleRange - (Math.PI + angleRange) / 2;
+    const x = SCREEN_WIDTH / 2 + radius * Math.cos(angle);
+    const y = 115 + radius * Math.sin(angle);
+    return [x, y];
+  });
+
+  const lineGenerator = line()
+    .x((d) => d[0])
+    .y((d) => d[1]);
+
+  return lineGenerator(points);
+};
+
+const generateDaggerShape = () => {
+  const baseWidth = 20;
+  const height = 25;
+  const holdHeight = 10;
+  const holdWidth = 10;
+  const yOffset = -40;
+
+  const points = [
+    [SCREEN_WIDTH / 2 - baseWidth / 2, 75 + yOffset],
+    [SCREEN_WIDTH / 2, 75 - height + yOffset],
+    [SCREEN_WIDTH / 2 + baseWidth / 2, 75 + yOffset],
+    [SCREEN_WIDTH / 2 + holdWidth / 2, 75 + yOffset],
+    [SCREEN_WIDTH / 2 + holdWidth / 2, 75 + holdHeight + yOffset],
+    [SCREEN_WIDTH / 2 - holdWidth / 2, 75 + holdHeight + yOffset],
+    [SCREEN_WIDTH / 2 - holdWidth / 2, 75 + yOffset],
+  ];
+
+  const lineGenerator = line()
+    .x((d) => d[0])
+    .y((d) => d[1]);
+
+  return lineGenerator(points) + "Z";
+};
+
+const generateWheelSegments = (segments) => {
+  const wheelSize = SCREEN_WIDTH - 50;
+  const radius = wheelSize / 2;
+  const cornerRadius = 5;
+  const arc = d3Arc()
+    .innerRadius(radius * 0.5)
+    .outerRadius(radius)
+    .cornerRadius(cornerRadius);
+  const segmentSpacing = 0.04;
+  const angleOffset = -Math.PI / segments.length;
+
+  return segments.map((amount, index) => {
+    const startAngle =
+      (2 * Math.PI * index) / segments.length +
+      segmentSpacing / 2 +
+      angleOffset;
+    const endAngle =
+      (2 * Math.PI * (index + 1)) / segments.length -
+      segmentSpacing / 2 +
+      angleOffset;
+
+    const rotationAngle = ((startAngle + endAngle) / 2) * (180 / Math.PI);
+
+    const pathData = arc({ startAngle, endAngle });
+    const centroid = arc.centroid({ startAngle, endAngle });
+
+    return { pathData, centroid, rotationAngle, amount };
+  });
+};
+
+export { generateBowShape, generateDaggerShape, generateWheelSegments };
